22627931e28b8ff6d563862fb49b4a0aecfe5d07
Maarten Nieber <hallomaarten@yahoo.com>
2021-01-13 19:22:51 +0100

    Extend make options

--- leap_mn/dockercompose/layer_configs.py
+++ leap_mn/dockercompose/layer_configs.py
@@ -2,4 +2,12 @@ def get_docker_compose_options(docker_compose):
     project = docker_compose.project
     suffix = "_dev" if docker_compose.is_dev else ""
     name = (project.name if project else "<INSERT NAME>") + suffix
-    return {"DOCKER_COMPOSE" + suffix.upper(): {"name": name}}
+    return {
+        "DOCKER_COMPOSE"
+        + suffix.upper(): {
+            "name": name,
+            "cwd": r"${/PROJECT/src_dir}",
+            "files": [docker_compose.name + ".yml"],
+        },
+        "ROOT": {"aliases": {"up": "docker-compose up --detach"}},
+    }
--- leap_mn/makefile/layer_configs.py
+++ leap_mn/makefile/layer_configs.py
@@ -1,2 +1,5 @@
 def get_make_options():
-    return dict(ROOT=dict(decorators=dict(docker=["make"])))
+    return dict(
+        ROOT=dict(decorators=dict(docker=["make"])),
+        MAKE=dict(cwd=r"${/SERVER/src_dir}"),
+    )
