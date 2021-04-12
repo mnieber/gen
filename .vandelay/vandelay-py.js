667e1266558ecae37b141894abb41013b30574e3
Maarten Nieber <hallomaarten@yahoo.com>
2021-04-12 09:04:43 +0200

    Add vandelay for moonleap itself

---
+++ .vandelay/vandelay-py.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/**
+ * Configuration file for VS Code Vandelay extension.
+ * https://github.com/ericbiewener/vscode-vandelay#configuration
+ */
+
+const basePath = "/home/maarten/projects/moonleap/src";
+
+module.exports = {
+  // This is the only required property. At least one path must be included.
+  includePaths: [basePath],
+};
