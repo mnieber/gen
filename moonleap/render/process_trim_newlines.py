564de1a39d41e820f8c6dbfb6151910352fb54bf
Maarten Nieber <hallomaarten@yahoo.com>
2021-04-14 15:55:50 +0200

    Use trim_newlines keep = #

--- moonleap/render/process_trim_newlines.py
+++ moonleap/render/process_trim_newlines.py
@@ -1,18 +1,23 @@
+import os
+import re
+
 import ramda as R
 
-trim_newlines_marker = "<<<<<<<<<<<<TRIM NEWLINES>>>>>>>>>>>>>>>"
-end_trim_newlines_marker = "<<<<<<<<<<<<END_TRIM NEWLINES>>>>>>>>>>>>>>>"
+end_trim_newlines_tag = "{% end_trim_newlines %}"
+
+
+def _find_trim_tag(text):
+    regex = r"{% trim_newlines keep=(?P<keep>[0-9]+) %}"
+    matches = list(re.finditer(regex, text, re.MULTILINE))
+    return int(matches[0].group("keep")) if matches else None
 
 
 def process_trim_newlines(lines):
     def t(x):
-        return (
-            trim_newlines_marker
-            if x == "{% trim_newlines %}"
-            else end_trim_newlines_marker
-            if x == "{% end_trim_newlines %}"
-            else x
-        )
+        keep = _find_trim_tag(x)
+        has_tag = x == end_trim_newlines_tag or keep is not None
+
+        return ("{% raw %}" + x + "{% endraw %}" + os.linesep) if has_tag else x
 
     return R.map(t, lines)
 
@@ -20,27 +25,35 @@ def process_trim_newlines(lines):
 def post_process_trim_newlines(lines):
     result = []
     trimming = False
-    prev_line = None
+    trimming_keep = None
+    empty_line_count = 0
     count = 0
 
     for line in lines:
-        if line == trim_newlines_marker:
+        keep = _find_trim_tag(line)
+
+        if keep is not None:
             count += 1
             trimming = True
-            prev_line = None
+            trimming_keep = keep
+            empty_line_count = 0
             continue
 
-        if line == end_trim_newlines_marker:
+        if line == end_trim_newlines_tag:
             count -= 1
             trimming = False
-            prev_line = None
+            trimming_keep = None
             continue
 
-        if trimming and line == "" and prev_line == "":
-            continue
+        if line.strip() == "":
+            if trimming and empty_line_count >= trimming_keep:
+                continue
+            else:
+                empty_line_count += 1
+        else:
+            empty_line_count = 0
 
         result.append(line)
-        prev_line = line
 
     if count != 0:
         raise Exception(
