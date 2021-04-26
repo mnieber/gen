2419f01d0e2acc16cc920e1848ac150aa3229f9d
Maarten Nieber <hallomaarten@yahoo.com>
2021-04-26 15:37:40 +0200

    Add scss files

--- moonleap_react_view/panel/__init__.py
+++ moonleap_react_view/panel/__init__.py
@@ -1,5 +1,5 @@
 import moonleap.resource.props as P
-from moonleap import MemFun, extend, rule, tags
+from moonleap import MemFun, extend, render_templates, rule, tags
 from moonleap.verbs import has
 from moonleap_react.component import Component
 from moonleap_react_view.frame.resources import Frame
@@ -16,5 +16,6 @@ def create_panel(term, block):
 
 @extend(Panel)
 class ExtendPanel:
+    render = MemFun(render_templates(__file__))
     frame = P.parent(Frame, has, "panel")
     create_router_configs = MemFun(props.create_router_configs)
