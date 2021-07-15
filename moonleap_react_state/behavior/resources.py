bf56a23ec211aa6d6d8694df4e7af8eb437e4d3d
Maarten Nieber <hallomaarten@yahoo.com>
2021-07-15 17:33:00 +0200

    Fix missing property

--- moonleap_react_state/behavior/resources.py
+++ moonleap_react_state/behavior/resources.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
 
 from moonleap import Resource
+from moonleap.utils.inflect import plural
 
 
 @dataclass
 class Behavior(Resource):
     name: str
     item_name: str
+
+    @property
+    def items_name(self):
+        return plural(self.item_name)
