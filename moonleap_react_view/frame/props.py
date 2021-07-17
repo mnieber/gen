f0cba0799eccb3f37b53c55d27b28a7b9d972d23
Maarten Nieber <hallomaarten@yahoo.com>
2021-07-17 20:24:44 +0200

    Fix frame router_configs

--- moonleap_react_view/frame/props.py
+++ moonleap_react_view/frame/props.py
@@ -1,7 +1,23 @@
+import ramda as R
+from moonleap import get_tweaks
+from moonleap_react.component.resources import get_component_base_url
 from moonleap_react_view.router import RouterConfig
 from moonleap_react_view.router.resources import reduce_router_configs
 
 
+def _get_route_params(self):
+    return R.path_or(
+        [],
+        ["services", self.module.service.name, "components", self.name, "route_params"],
+    )(get_tweaks())
+
+
 def create_router_configs(self):
-    result = reduce_router_configs([RouterConfig(component=self, url="", wraps=True)])
+    base_url = get_component_base_url(self, "")
+    url = "/".join(
+        ([base_url] if base_url else [])
+        + [":" + x for x in _get_route_params(self) if x is not None]
+    )
+    router_config = RouterConfig(component=self, url=url, wraps=True)
+    result = reduce_router_configs([router_config])
     return result
