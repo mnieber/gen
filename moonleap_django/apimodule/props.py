76c02d3be5eaaea55aee65524a09573940a3c411
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-01 17:57:13 +0200

    Add Mutation to api:module schema.py

--- moonleap_django/apimodule/props.py
+++ moonleap_django/apimodule/props.py
@@ -2,8 +2,9 @@ def _modules(self):
     return [x for x in self.django_app.modules if x.item_types]
 
 
-def p_section_base_classes(self):
-    return "".join([f"{x.name_snake}.schema.Query, " for x in _modules(self)])
+def p_section_base_classes(self, postfix):
+    result = "".join([f"{x.name_snake}.schema.{postfix}, " for x in _modules(self)])
+    return (result + "graphene.ObjectType") if result else ""
 
 
 def p_section_imports(self):
