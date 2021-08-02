7e5c80340f00feb646822cb27d668177f2e500d8
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-02 09:31:43 +0200

    Fix django packages

--- moonleap_django/djangoapp_and_postgres/__init__.py
+++ moonleap_django/djangoapp_and_postgres/__init__.py
@@ -9,9 +9,7 @@ from . import makefile_rules
 
 @rule("django-app", connects, "postgres:service")
 def django_uses_postgres_service(django_app, postgres_service):
-    add(
-        django_app, PkgDependency(["postgresql-client", "psycopg2-binary"], is_dev=True)
-    )
+    add(django_app, PkgDependency(["postgresql-client"], is_dev=True))
     add(django_app, PipRequirement(["psycopg2"]))
     add(django_app, PipDependency(["pgcli==2.1.1"], is_dev=True))
     add(django_app, makefile_rules.get_postgres())
