ad586d0749c0e7656f0f02e0bd2649fa38bcbbe8
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-19 10:35:14 +0200

    Rename to baseDir

--- .vandelay/vandelay-py.js
+++ .vandelay/vandelay-py.js
@@ -4,9 +4,9 @@
  * https://github.com/ericbiewener/vscode-vandelay#configuration
  */
 
-const basePath = "/home/maarten/projects/moonleap/src";
+const baseDir = '/home/maarten/projects/moonleap/src';
 
 module.exports = {
   // This is the only required property. At least one path must be included.
-  includePaths: [basePath],
+  includePaths: [baseDir],
 };
