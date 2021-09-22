2598b4d7409777b8a8fdd8a0873da54dc9af9113
Maarten Nieber <hallomaarten@yahoo.com>
2021-09-22 15:54:41 +0200

    WIP

--- titan/react_module_pkg/form/__init__.py
+++
@@ -1,9 +0,0 @@
-from moonleap import kebab_to_camel, create
-
-from .resources import Form
-
-
-@create(["form"])
-def create_form(term, block):
-    form = Form(name=kebab_to_camel(term.data) + "Form")
-    return form
