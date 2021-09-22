cfcdab39db121b90ae21ed57fee59d3dc9142ee7
Maarten Nieber <hallomaarten@yahoo.com>
2021-09-22 17:51:11 +0200

    Rename to field_name

--- moonleap/resources/type_spec.py
+++ moonleap/resources/type_spec.py
@@ -13,14 +13,14 @@ class TypeSpec:
 
 
 def add_related_set_field_to_type_spec(type_spec, field_spec, fk_type_spec):
-    name = lower0(type_spec.type_name + "Set")
-    if [x for x in fk_type_spec.field_specs if x.name == name]:
+    field_name = lower0(type_spec.type_name + "Set")
+    if [x for x in fk_type_spec.field_specs if x.name == field_name]:
         raise Exception("Field spec with name {name} already exists")
 
     fk_type_spec.field_specs.append(
         FieldSpec(
-            name=name,
-            name_snake=camel_to_snake(name),
+            name=field_name,
+            name_snake=camel_to_snake(field_name),
             required=False,
             private=field_spec.private,
             field_type="related_set",
