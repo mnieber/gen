ae7cac4f2fd8e2a898aaba2ad3cca927177c9c6f
Maarten Nieber <hallomaarten@yahoo.com>
2022-10-09 15:50:40 +0200

    Add format-tr command

---
+++ extra/format_tr.py
@@ -0,0 +1,30 @@
+def format_tr(fn):
+    with open(fn) as ifs:
+        lines = ifs.readlines()
+
+    blocks = []
+    block = None
+    for line in lines:
+        if not block and line.rstrip().endswith(": {"):
+            block = [line]
+        elif block:
+            block.append(line)
+            if line.rstrip().removesuffix(",").endswith("}"):
+                blocks.append(block)
+                block = None
+
+    blocks = sorted(blocks, key=lambda block: block[0])
+    result = ["page = {\n"]
+    for block in blocks:
+        result.extend(block)
+    result.append("}\n")
+
+    if lines != result:
+        with open(fn, "w") as f:
+            f.write("".join(result))
+
+
+if __name__ == "__main__":
+    import sys
+
+    format_tr(sys.argv[1])
---
+++ extra/moonleap_dodo_commands/format-tr.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+
+from dodo_commands import CommandError, Dodo
+
+
+def _args():
+    # Create the parser
+    parser = ArgumentParser(description="")
+    parser.add_argument("fn")
+
+    # Use the parser to create the command arguments
+    args = Dodo.parse_args(parser, config_args=[])
+    args.cwd = Dodo.get("/ROOT/src_dir")
+
+    # Raise an error if something is not right
+    if False:
+        raise CommandError("Oops")
+
+    return args
+
+
+# Use safe=False if the script makes changes other than through Dodo.run
+if Dodo.is_main(__name__, safe=True):
+    args = _args()
+    Dodo.run(
+        ["python", "./extra/format_tr.py", args.fn],
+        cwd=args.cwd,
+    )
