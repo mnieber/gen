ef4b5e05eca7570bf22a9c81f23a479e6c807717
Maarten Nieber <hallomaarten@yahoo.com>
2022-11-23 20:52:12 +0100

    Rename states.yaml to pipelines.yaml

--- titan/react_view_pkg/stateprovider/props.py
+++ titan/react_view_pkg/stateprovider/props.py
@@ -6,38 +6,38 @@ from moonleap.verbs import connects, has, provides
 
 def state_provider_load(state_provider):
     spec_dir = get_session().spec_dir
-    fn = os.path.join(spec_dir, "states.yaml")
+    fn = os.path.join(spec_dir, "pipelines.yaml")
     if os.path.exists(fn):
-        state_provider_datas = load_yaml(fn).get("stateProviders", {})
-        return _get_forwards(state_provider, state_provider_datas)
+        component_datas = load_yaml(fn).get("components", {})
+        return _get_forwards(state_provider, component_datas)
 
 
-def _get_forwards(state_provider, state_provider_datas):
+def _get_forwards(state_provider, component_datas):
     forwards = []
-    for state_provider_term, state_provider_data in state_provider_datas.items():
-        _check_name(state_provider_term)
-        if state_provider_term == state_provider.meta.term.as_normalized_str():
-            _get_state_provider(state_provider_term, state_provider_data, forwards)
+    for component_term, component_data in component_datas.items():
+        _check_name(component_term)
+        if component_term == state_provider.meta.term.as_normalized_str():
+            _get_state_provider(component_term, component_data, forwards)
     return forwards
 
 
-def _get_state_provider(state_provider_term, state_provider_data, forwards):
-    pipeline_datas = state_provider_data.get("pipelines", {})
+def _get_state_provider(component_term, component_data, forwards):
+    pipeline_datas = component_data.get("pipelines", {})
     for pipeline_term, pipeline_data in pipeline_datas.items():
         _check_name(pipeline_term)
-        forwards.append(create_forward(state_provider_term, has, pipeline_term))
+        forwards.append(create_forward(component_term, has, pipeline_term))
         _get_pipeline(pipeline_term, pipeline_data, forwards)
 
-    state_datas = state_provider_data.get("states", {})
+    state_datas = component_data.get("states", {})
     for state_term, state_data in state_datas.items():
         _check_name(state_term)
-        forwards.append(create_forward(state_provider_term, provides, state_term))
+        forwards.append(create_forward(component_term, provides, state_term))
         _get_state(state_term, state_data, forwards)
 
-    items = state_provider_data.get("items_and_lists", [])
+    items = component_data.get("items_and_lists", [])
     for item_term in items:
         _check_name(item_term)
-        forwards.append(create_forward(state_provider_term, provides, item_term))
+        forwards.append(create_forward(component_term, provides, item_term))
 
 
 def _get_pipeline(pipeline_term, pipeline_data, forwards):
