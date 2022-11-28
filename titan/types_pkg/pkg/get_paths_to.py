fbbd0f246d927afd00a41be9ce3247a9a9591d5f
Maarten Nieber <hallomaarten@yahoo.com>
2022-11-28 17:50:23 +0100

    Document get_paths_to

--- titan/types_pkg/pkg/get_paths_to.py
+++ titan/types_pkg/pkg/get_paths_to.py
@@ -1,14 +1,27 @@
 from moonleap.utils.fp import uniq
 from titan.api_pkg.pkg.api_spec import ApiSpec
 
+# This function returns different paths from type_spec_or_api_spec to
+# an fk field of type "type_name", e.g. the path Foo.bar.baz goes
+# from the Foo type-spec to the baz field via the bar field.
+#
+# Why is this needed? Because when we reorder a list of items in the frontend,
+# then for performance reasons we would like to immediately apply the same
+# reordering to all data-structures in the frontend that have a similar list.
+# E.g. when we reorder todolist.todos, then any data-structure that has a
+# todolist.todos field should also be reordered.
 
-def get_paths_to(type_name, type_spec, base_path, skip=None):
+
+def get_paths_to(type_name, type_spec_or_api_spec, base_path, skip=None):
     if skip is None:
         skip = []
 
-    is_api_spec = isinstance(type_spec, ApiSpec)
+    is_api_spec = isinstance(type_spec_or_api_spec, ApiSpec)
+    type_spec = type_spec_or_api_spec if not is_api_spec else None
+    api_spec = type_spec_or_api_spec if is_api_spec else None
+
     new_skip = skip
-    if not is_api_spec:
+    if type_spec:
         if type_spec.type_name in skip:
             return []
         else:
@@ -18,7 +31,7 @@ def get_paths_to(type_name, type_spec, base_path, skip=None):
     infix = "." if base_path else ""
 
     field_specs = (
-        type_spec.get_outputs(["fk", "relatedSet"])
+        api_spec.get_outputs(["fk", "relatedSet"])
         if is_api_spec
         else type_spec.get_field_specs(["fk", "relatedSet"])
     )
