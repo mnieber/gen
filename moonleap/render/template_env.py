42d9b9d29a7c6f05f8a22238b5dc767c83ca111a
Maarten Nieber <hallomaarten@yahoo.com>
2022-11-30 16:54:48 +0100

    Add get_template_from_str

--- moonleap/render/template_env.py
+++ moonleap/render/template_env.py
@@ -1,6 +1,8 @@
+import io
 import os
 
 import jinja2
+
 from moonleap.render.transforms import get_transforms
 from moonleap.utils import chop
 
@@ -9,20 +11,26 @@ _template_by_fn = dict()
 
 def print_last_template(template_fn):
     print("Template: %s" % template_fn)
-    print(os.linesep.join(_template_by_fn[str(template_fn)]))
+    print(os.linesep.join(_template_by_fn.get(str(template_fn), [])))
 
 
 def load_template(template_fn):
     with open(template_fn) as ifs:
-        lines = [chop(x) for x in ifs.readlines()]
+        return get_template_from_stream(ifs, template_fn)
+
+
+def get_template_from_stream(ifs, template_fn=None):
+    lines = [chop(x) for x in ifs.readlines()]
 
     transformed_lines = lines
     for t in get_transforms():
         transformed_lines = t(transformed_lines, template_fn)
 
-    _template_by_fn[str(template_fn)] = [
-        "%3d: %s" % (i + 1, transformed_lines[i]) for i in range(len(transformed_lines))
-    ]
+    if template_fn:
+        _template_by_fn[str(template_fn)] = [
+            "%3d: %s" % (i + 1, transformed_lines[i])
+            for i in range(len(transformed_lines))
+        ]
 
     return os.linesep.join(transformed_lines)
 
@@ -38,6 +46,11 @@ template_env = jinja2.Environment(
 
 def get_template(fn):
     return template_env.get_template(str(fn))
+
+
+def get_template_from_str(template_str):
+    with io.StringIO(template_str) as ifs:
+        return template_env.from_string(get_template_from_stream(ifs))
 
 
 def add_filter(name, f):
