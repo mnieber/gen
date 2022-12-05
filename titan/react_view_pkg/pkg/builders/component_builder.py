58baa547f5337f5cfd0f1203bd268a18d1ab505a
Maarten Nieber <hallomaarten@yahoo.com>
2022-12-05 14:11:44 +0100

    Fix ComponentBuilder

--- titan/react_view_pkg/pkg/builders/component_builder.py
+++ titan/react_view_pkg/pkg/builders/component_builder.py
@@ -3,8 +3,9 @@ from titan.react_view_pkg.pkg.builder import Builder
 
 class ComponentBuilder(Builder):
     def build(self):
-        attrs = []
         component = self.widget_spec.component
+
+        attrs = list(self.widget_spec.div_props)
         for pipeline in component.pipelines:
             if pipeline.root_props:
                 named_input = pipeline.elements[0].obj
@@ -12,8 +13,8 @@ class ComponentBuilder(Builder):
                 # The required prop must be supplied by some parent component
                 data_path = self.parent_builder.get_data_path(named_input)
                 attrs += [f"{required_prop_name}={{{data_path}}}"]
-
         attrs_str = " ".join(attrs)
+
         key = self.widget_spec.div_key
         key_attr = f"key={{{key}}}" if key else ""
         self.add_lines([f"<{self.output.widget_class_name} {key_attr} {attrs_str}/>"])
