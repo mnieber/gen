efc771308cf74bc7cf9d2edbedb8cf96a02cab6d
Maarten Nieber <hallomaarten@yahoo.com>
2022-12-10 11:36:27 +0100

    Add form fields imports

--- titan/react_view_pkg/pkg/builders/form_fields_builder/tpl.py
+++ titan/react_view_pkg/pkg/builders/form_fields_builder/tpl.py
@@ -1,39 +1,64 @@
 from moonleap import Tpls, chop0
 
+form_fields_imports_tpl = chop0(
+    """
+{% magic_with __.mutation.name as postMyMutation %}
+import * as R from 'ramda';
+import {
+  FormStateProvider,
+  HandleValidateArgsT,
+  HandleSubmitArgsT,
+  unflatten
+} from 'react-form-state-context';
+import { usePostMyMutation } from 'src/api/mutations';
+import {
+  ControlledCheckbox,
+  Field,
+  GlobalError,
+  TextField,
+  SaveButton,
+  SlugField,
+  UpdateSlugButton,
+  ValuePickerField
+} from 'src/forms/components';
+{% end_magic_with %}
+  """
+)
+
 form_fields_tpl = chop0(
     """
-          <GlobalError />
-          {{ form_fields_block }}
-          <SaveButton
-            label="Save"
-            disabled={false}
-          />
+    <GlobalError />
+    {{ form_fields_block }}
+    <SaveButton
+      label="Save"
+      disabled={false}
+    />
     """
 )
 
 form_field_tpl = chop0(
     """
-          <Field
-            fieldName="{{ name }}"
-            label="{{ __.label(name) }}"
-            buttons={[                                                                                  {% if field_spec.field_type in ("slug",) %}
-              <UpdateSlugButton
-                key="1"
-                relatedFieldName="{{ __.slug_src(field_spec) }}"
-              />
-            ]}                                                                                          {% endif %}
-          >
-            <TextField controlled={true} />                                                             {% ?? field_spec.field_type in ("string", "text", "url") %}
-            <SlugField />                                                                               {% ?? field_spec.field_type in ("slug",) %}
-            <ValuePickerField                                                                           {% if field_spec.field_type in ("uuid",) and field_spec.target %}
-              isCreatable={false}
-              isMulti={false}
-              pickableValues={props.fieldSpecTargets}
-              pickableValue={initialValues['{{ name }}']}
-              labelFromValue={(x: any) => x.{{ __.display_field_name(field_spec.target_type_spec) }}}
-            />                                                                                          {% endif %}
-            <div className="flex flex-row items-begin mt-1"><ControlledCheckbox /></div>                {% ?? field_spec.field_type in ("boolean",) %}
-          </Field>
+    <Field
+      fieldName="{{ name }}"
+      label="{{ __.label(name) }}"
+      buttons={[                                                                                  {% if field_spec.field_type in ("slug",) %}
+        <UpdateSlugButton
+          key="1"
+          relatedFieldName="{{ __.slug_src(field_spec) }}"
+        />
+      ]}                                                                                          {% endif %}
+    >
+      <TextField controlled={true} />                                                             {% ?? field_spec.field_type in ("string", "text", "url") %}
+      <SlugField />                                                                               {% ?? field_spec.field_type in ("slug",) %}
+      <ValuePickerField                                                                           {% if field_spec.field_type in ("uuid",) and field_spec.target %}
+        isCreatable={false}
+        isMulti={false}
+        pickableValues={props.fieldSpecTargets}
+        pickableValue={initialValues['{{ name }}']}
+        labelFromValue={(x: any) => x.{{ __.display_field_name(field_spec.target_type_spec) }}}
+      />                                                                                          {% endif %}
+      <div className="flex flex-row items-begin mt-1"><ControlledCheckbox /></div>                {% ?? field_spec.field_type in ("boolean",) %}
+    </Field>
   """
 )
 
@@ -41,6 +66,7 @@ form_fields_hooks_tpl = chop0(
     """
 {% magic_with __.mutation.name as postMyMutation %}
   const postMyMutation = usePostMyMutation().mutateAsync;
+  {{ "" }}
 {% end_magic_with %}
   """
 )
@@ -66,6 +92,7 @@ form_fields_preamble_tpl = chop0(
         values                                                                                          {% else %}{% endif %}
       )).then(() => formState.reset(initialValues, initialErrors));
     };
+    {{ ""}}
 {% end_magic_with %}
   """
 )
@@ -76,4 +103,5 @@ tpls = Tpls(
     form_fields_tpl=form_fields_tpl,
     form_field_tpl=form_field_tpl,
     form_fields_hooks_tpl=form_fields_hooks_tpl,
+    form_fields_imports_tpl=form_fields_imports_tpl,
 )
