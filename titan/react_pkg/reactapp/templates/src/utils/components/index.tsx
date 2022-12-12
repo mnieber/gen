1b768eef95b312726a1f04c4878042615380793e
Maarten Nieber <hallomaarten@yahoo.com>
2022-12-12 19:31:12 +0100

    Some changes in src/utils

--- titan/react_pkg/reactapp/templates/src/utils/components/index.tsx
+++ titan/react_pkg/reactapp/templates/src/utils/components/index.tsx
@@ -1,2 +1,1 @@
-export { Effect, EffectWithoutArgs } from "./Effect";
-
+export { Effect } from './Effect';
---
+++ titan/react_pkg/reactapp/templates/src/utils/hooks/useSearchAndUrlParams.tsx
@@ -0,0 +1,10 @@
+import { merge } from 'ramda';
+import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'src/utils/hooks/useSearchParams';
+import { ObjT } from 'src/utils/types';
+
+export const useSearchAndUrlParams = (): ObjT => {
+  const params = useParams();
+  const { all: searchParams } = useSearchParams();
+  return merge(params, searchParams);
+};
