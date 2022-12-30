23316310d157386df2a2b77dd9a463a438fa62df
Maarten Nieber <hallomaarten@yahoo.com>
2022-12-30 14:28:53 +0100

    Fix: icon builder

--- titan/react_view_pkg/pkg/builders/icon_builder.py
+++ titan/react_view_pkg/pkg/builders/icon_builder.py
@@ -1,7 +1,12 @@
+from moonleap import u0
 from titan.react_view_pkg.pkg.builder import Builder
 
 
 class IconBuilder(Builder):
     def build(self):
-        name = self.widget_spec.values["name"]
+        name = u0(self.widget_spec.values["name"])
         self.output.set_flags([f"frames/{name}Icon"])
+        self.add(
+            lines=[f"<{name}Icon  />"],
+            imports=[f"import {{ {name}Icon }} from 'src/frames/icons/{name}Icon';"],
+        )
