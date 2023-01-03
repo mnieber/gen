b435236d0492d407662cd0f87fdfbd31641d6bee
Maarten Nieber <hallomaarten@yahoo.com>
2023-01-03 23:02:24 +0100

    Use useValuePickerState

---
+++ pkgs/titan/react_pkg/reactapp/templates/src/utils/hooks/useValuePickerState.index.ts
@@ -0,0 +1,1 @@
+export { useValuePickerState } from './useValuePickerState';
---
+++ pkgs/titan/react_pkg/reactapp/templates/src/utils/hooks/useValuePickerState.ts
@@ -0,0 +1,47 @@
+import { when } from 'mobx';
+import React from 'react';
+import { Highlight } from 'skandha-facets/Highlight';
+import { Selection } from 'skandha-facets/Selection';
+
+export type PropsT<ValueT> = {
+  highlight?: Highlight;
+  selection?: Selection;
+  updateUrl?: (value: ValueT) => void;
+};
+
+export const useValuePickerState = <ValueT extends { id: string }>(
+  props: PropsT<ValueT>
+) => {
+  const [isUpdating, setIsUpdating] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  const setItem = React.useMemo(
+    () => (value: ValueT) => {
+      props.highlight && props.highlight.highlightItem(value.id);
+      props.selection && props.selection.selectItem({ itemId: value.id });
+      props.updateUrl && props.updateUrl(value);
+    },
+    [props]
+  );
+
+  const setItemWhenReady = (items: ValueT[], id: string) => {
+    let item: ValueT | undefined = undefined;
+    when(
+      () => !!(item = items.find((x: ValueT) => x.id === id)),
+      () => {
+        item && setItem(item);
+        setIsUpdating(false);
+        setIsError(false);
+      },
+      {
+        timeout: 10 || 5000,
+        onError: () => {
+          setIsUpdating(false);
+          setIsError(true);
+        },
+      }
+    );
+  };
+
+  return { isUpdating, setIsUpdating, isError, setItem, setItemWhenReady };
+};
