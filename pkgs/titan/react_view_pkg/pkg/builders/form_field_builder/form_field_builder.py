6d3ad202664af8417dd962c028baba2305f9efad
Maarten Nieber <hallomaarten@yahoo.com>
2023-01-14 05:22:36 +0100

    Refactor form_field_builder

--- pkgs/titan/react_view_pkg/pkg/builders/form_field_builder/form_field_builder.py
+++ pkgs/titan/react_view_pkg/pkg/builders/form_field_builder/form_field_builder.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-from moonleap import get_tpl
 from titan.react_view_pkg.pkg.add_tpl_to_builder import add_tpl_to_builder
 from titan.react_view_pkg.pkg.builder import Builder
+
+from moonleap import get_tpl
 
 
 class FormFieldBuilder(Builder):
@@ -11,7 +12,7 @@ class FormFieldBuilder(Builder):
         context = dict(
             field_spec=field_spec,
             name=self.widget_spec.values.get("form_field_name"),
-            __=self.widget_spec.values.get("helper"),
+            **self.widget_spec.values.get("parent_context"),
         )
         tpl = get_tpl(Path(__file__).parent / "tpl.tsx.j2", context)
         add_tpl_to_builder(tpl, self)
--- pkgs/titan/react_view_pkg/pkg/builders/form_fields_builder/form_fields_builder.py
+++ pkgs/titan/react_view_pkg/pkg/builders/form_fields_builder/form_fields_builder.py
@@ -4,10 +4,11 @@ from pathlib import Path
 import ramda as R
 from titan.react_view_pkg.pkg.add_tpl_to_builder import add_tpl_to_builder
 from titan.react_view_pkg.pkg.builder import Builder
+from titan.react_view_pkg.pkg.builders.form_state_provider_builder.get_fields import (
+    get_fields,
+)
 
-from moonleap import get_tpl
-
-from .helper import Helper
+from moonleap import get_tpl, u0
 
 
 class FormFieldsBuilder(Builder):
@@ -16,30 +17,59 @@ class FormFieldsBuilder(Builder):
             return {f"Field with FormField": "pass"}
 
     def build(self):
-        component = self.widget_spec.root.component
-        __ = self.__ = Helper(
-            item_name=self.ih.array_item_name,
-            mutation=R.head(component.mutations),
-            fields=self.widget_spec.values.get("fields"),
-        )
+        context = self.get_context()
+
         field_widget_spec = self.widget_spec.get_place("Field")
         lines = []
-        for form_field_name, field_spec in __.fields:
+        for form_field_name, field_spec in context["fields"]:
             build_output = self._get_field_widget_output(
-                form_field_name, field_widget_spec, field_spec
+                form_field_name, field_widget_spec, field_spec, context
             )
             lines.extend(build_output.lines)
             self.output.graft(build_output)
+        context["form_fields_block"] = os.linesep.join(lines)
 
-        context = dict(__=__, __form_fields_block=os.linesep.join(lines))
         tpl = get_tpl(Path(__file__).parent / "tpl.tsx.j2", context)
         add_tpl_to_builder(tpl, self)
 
-    def _get_field_widget_output(self, form_field_name, field_widget_spec, field_spec):
+    def get_context(self):
+        component = self.widget_spec.root.component
+        item_name = self.ih.array_item_name
+        mutation = R.head(component.mutations).api_spec
+        fields = (
+            get_fields(mutation, self.widget_spec.values.get("fields"))
+            if mutation
+            else []
+        )
+
+        return dict(
+            item_name=item_name,
+            fields=fields,
+            get_slug_src=_get_slug_src,
+            get_label=_get_label,
+            get_display_field_name=_get_display_field_name,
+        )
+
+    def _get_field_widget_output(
+        self, form_field_name, field_widget_spec, field_spec, context
+    ):
         from titan.react_view_pkg.pkg.build import build
 
         with field_widget_spec.memo():
             field_widget_spec.values["form_field_name"] = form_field_name
             field_widget_spec.values["field_spec"] = field_spec
-            field_widget_spec.values["helper"] = self.__
+            field_widget_spec.values["parent_context"] = context
             return build(field_widget_spec)
+
+
+def _get_slug_src(fields, field_spec):
+    slug_sources = [name for name, field_spec in fields if field_spec.is_slug_src]
+    return R.head(slug_sources) or "Moonleap Todo: slug_src"
+
+
+def _get_label(name):
+    return u0(name.replace(".", " "))
+
+
+def _get_display_field_name(type_spec):
+    return type_spec.display_field.name if type_spec.display_field else "id"
