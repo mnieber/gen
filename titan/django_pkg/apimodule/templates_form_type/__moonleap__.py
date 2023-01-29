a8b06fda696c02f336ea2d96d79d6906c61f0219
Maarten Nieber <hallomaarten@yahoo.com>
2023-01-29 11:11:00 +0100

    Use if "server" in x.has_api

--- titan/django_pkg/apimodule/templates_form_type/__moonleap__.py
+++ titan/django_pkg/apimodule/templates_form_type/__moonleap__.py
@@ -1,6 +1,8 @@
 def get_helpers(_):
     class Helpers:
-        field_specs = _.form_type_spec.get_field_specs()
+        field_specs = [
+            x for x in _.form_type_spec.get_field_specs() if "server" in x.has_api
+        ]
 
         def args(self, field_spec):
             return "required=False" if field_spec.is_optional("server") else ""
