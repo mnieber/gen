833c0f9fa17599b5e1e526c3002c16a2c7d6e6d8
Maarten Nieber <hallomaarten@yahoo.com>
2023-03-12 22:38:03 +0100

    Add "client" to has_api of fields in outputs_type_spec

--- titan/api_pkg/pkg/load_api_specs.py
+++ titan/api_pkg/pkg/load_api_specs.py
@@ -8,9 +8,13 @@ from titan.typespec.get_api_spec import get_api_spec
 def load_api_specs(api_reg, spec_dir):
     fn = os.path.join(spec_dir, "api.yaml")
     if os.path.exists(fn):
-        get_all_api_specs(api_reg, "server", load_yaml(fn))
+        get_all_api_specs(api_reg, load_yaml(fn))
 
 
-def get_all_api_specs(api_reg, host, root_api_spec_dict):
+def get_all_api_specs(api_reg, root_api_spec_dict):
     for key, api_spec_dict in root_api_spec_dict.items():
-        get_api_spec(api_reg, host, key, api_spec_dict, get_type_reg().type_names)
+        api_spec = get_api_spec(
+            api_reg, "server", key, api_spec_dict, get_type_reg().type_names
+        )
+        for output_field_spec in api_spec.outputs_type_spec.field_specs:
+            output_field_spec.has_api.append("client")
