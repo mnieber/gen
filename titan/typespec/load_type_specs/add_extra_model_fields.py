8da904afa0dc3b2871d94a23f716fc145952a198
Maarten Nieber <hallomaarten@yahoo.com>
2023-08-25 09:15:34 +0200

    Insert extra model fields at the top

--- titan/typespec/load_type_specs/add_extra_model_fields.py
+++ titan/typespec/load_type_specs/add_extra_model_fields.py
@@ -1,14 +1,32 @@
+from moonleap.utils.fp import append_uniq
+
+
 def add_extra_model_fields(type_spec, value):
+    new_value = dict()
     if (
         type_spec.is_entity
         and not type_spec.get_field_spec_by_key("id")
         and not value.get("id")
     ):
-        value["id"] = "Id,primary_key"
+        new_value["id"] = "Id,primary_key"
 
     if (
         type_spec.is_sorted
         and not type_spec.get_field_spec_by_key("sortPos")
         and not value.get("sortPos")
     ):
-        value["sortPos"] = "Int = 0"
+        new_value["sortPos"] = "Int = 0"
+
+    if not new_value:
+        return
+
+    new_value.update(value)
+    if new_value.get("sortPos"):
+        api_spec = new_value.setdefault("__api__", {})
+        deleted_fields = api_spec.setdefault("__delete__", [])
+        append_uniq(deleted_fields, "sortPos")
+
+    for key in list(value.keys()):
+        del value[key]
+
+    value.update(new_value)
