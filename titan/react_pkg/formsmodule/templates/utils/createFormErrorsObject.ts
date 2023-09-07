7b9976101125adae4008a0fcb392a5276024eb55
Maarten Nieber <hallomaarten@yahoo.com>
2023-09-07 17:20:10 +0200

    WIP update utils

--- titan/react_pkg/formsmodule/templates/utils/createFormErrorsObject.ts
+++ titan/react_pkg/formsmodule/templates/utils/createFormErrorsObject.ts
@@ -3,6 +3,12 @@ import { ObjT } from '/src/utils/types';
 
 export const createFormErrorsObject = (): ObjT => {
   const formErrors: ObjT = {};
+  // This line ensures that the formErrors object can be decoded. The formErrors object
+  // may have circular references, therefore, we only keep the keys.
   formErrors.toJSON = () => JSON.stringify(R.keys(formErrors));
   return formErrors;
+};
+
+export const getErrorKeys = (formErrors: ObjT) => {
+  return R.keys(formErrors).filter((x) => x !== 'toJSON');
 };
--- titan/react_pkg/reactapp/templates/src/utils/hooks/index.ts
+++ titan/react_pkg/reactapp/templates/src/utils/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useBuilder } from './useBuilder';
 export { useRingIdx } from './useRingIdx';
 export { useSearchAndUrlParams } from './useSearchAndUrlParams';
 export { useSearchParams } from './useSearchParams';
