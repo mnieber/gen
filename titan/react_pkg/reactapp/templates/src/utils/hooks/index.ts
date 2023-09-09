1ce09b5d76e5688286b927c9d3c9cc8cb119d4c2
Maarten Nieber <hallomaarten@yahoo.com>
2023-09-09 12:14:54 +0200

    Drop useRingIdx

--- titan/react_pkg/reactapp/templates/src/utils/hooks/index.ts
+++ titan/react_pkg/reactapp/templates/src/utils/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useBuilder } from './useBuilder';
-export { useRingIdx } from './useRingIdx';
 export { useSearchAndUrlParams } from './useSearchAndUrlParams';
 export { useSearchParams } from './useSearchParams';
