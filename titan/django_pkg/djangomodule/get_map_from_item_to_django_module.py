3b191087364a3cbfee392d327b880e7170b54772
Maarten Nieber <hallomaarten@yahoo.com>
2023-11-04 20:46:34 +0100

    Don't raise Exception if Module not found

--- titan/django_pkg/djangomodule/get_map_from_item_to_django_module.py
+++ titan/django_pkg/djangomodule/get_map_from_item_to_django_module.py
@@ -2,6 +2,7 @@ import typing as T
 from dataclasses import dataclass
 
 import ramda as R
+from moonleap import get_session
 
 
 @dataclass
@@ -26,9 +27,9 @@ def get_map_from_item_to_django_module(type_reg, modules):
         if module_name and not django_module:
             django_module = R.head([x for x in modules if x.name == module_name])
 
-        if not django_module:
-            raise Exception(f"Module not found: {module_name}")
-
-        lut[type_name] = Data(django_module=django_module, item=item)
+        if django_module:
+            lut[type_name] = Data(django_module=django_module, item=item)
+        else:
+            get_session().warn(f"Module not found: {module_name}")
 
     return lut
